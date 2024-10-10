09287041de2e379a2bd38e2d1c6767e5cc3eb7f6
Sir Qasim <m.qasim077@gmail.com>
2024-10-10 16:51:09 +0500

    Add files via upload

--- 04_LangChain_LanGraph/01_LangChain/01_Basic/main.py
+++ 04_LangChain_LanGraph/01_LangChain/01_Basic/main.py
@@ -3,13 +3,12 @@ from fastapi import FastAPI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
-import getpass
-import os
-from langchain_google_genai import ChatGoogleGenerativeAI
-import uvicorn
-
 # Load environment variables from the .env file
 load_dotenv()
+
+
+
+
 
 # 1. Create prompt template
 system_template = "Translate the following into {language}:"
@@ -19,9 +18,17 @@ prompt_template = ChatPromptTemplate.from_messages([
 ])
 
 # 2. Create model
+import getpass
+import os
+
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", api_key=GEMINI_API_KEY)
-model = llm  # llm we created in cell number 1
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", api_key = GEMINI_API_KEY)
+model = llm # llm we created in cell number 1
 
 # 3. Create parser
 parser = StrOutputParser()
@@ -29,16 +36,19 @@ parser = StrOutputParser()
 # 4. Create chain
 chain = prompt_template | model | parser
 
-# 5. App definition
+
+# 4. App definition
 app = FastAPI(
-    title="LangChain Server",
-    version="1.0",
-    description="A simple API server using LangChain's Runnable interfaces",
+  title="LangChain Server",
+  version="1.0",
+  description="A simple API server using LangChain's Runnable interfaces",
 )
 
 @app.get("/llm")
-async def root(lang: str, text: str):
+async def root(lang, text):
     return chain.invoke({"text": text, "language": lang})
 
 if __name__ == "__main__":
+    import uvicorn
+
     uvicorn.run(app, host="localhost", port=8000)
